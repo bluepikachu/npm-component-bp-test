f1e5c8208d081d5fb4645983ffbc6a8e498d231b
Bluepikachu <vivianbefree@gmail.com>
2024-03-13 09:51:23 +0800

    new project

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// Import all the components here, there are more elegant ways to do this but this is the simplest
+import PersonalButton from "./components/PersonalButton.vue";
+import PersonalTable from "./components/PersonalTable.vue";
+import PersonalInput from "./components/PersonalInput.vue";
+
+export { PersonalButton, PersonalTable, PersonalInput };
